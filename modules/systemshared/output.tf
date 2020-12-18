305591a8ed064c6a3d3d9a1d78d52cc7df746d01
Florian Lauchner <florian.lauchner@gmail.com>
2020-12-18 14:46:27 +0100

    role output changed

--- modules/systemshared/output.tf
+++ modules/systemshared/output.tf
@@ -2,18 +2,18 @@ output "backup_plan_filesystems" {
   value = aws_backup_plan.filesystems.id
 }
 
-output "iam_backup_role_id" {
-  value = aws_iam_role.backup.id
+output "iam_backup_role" {
+  value = aws_iam_role.backup.arn
 }
 
-output "iam_s4_role_id" {
-  value = aws_iam_role.s4_role.id
+output "iam_s4_role" {
+  value = aws_iam_role.s4_role.name
 }
 
-output "iam_hanadb_role_id" {
-  value = aws_iam_role.hanadb_role.id
+output "iam_hanadb_role" {
+  value = aws_iam_role.hanadb_role.name
 }
 
-output "kms_key_arn" {
+output "kms_key" {
   value = aws_kms_key.system.arn
 }
