8e8f1e6e7d38b3feedf1d753a977670e2e2cd62e
Florian Lauchner <florian.lauchner@gmail.com>
2020-12-18 14:48:40 +0100

    Creation of ansible inventory from template file

---
+++ ansible.tf
@@ -0,0 +1,14 @@
+# Ansible inventory
+resource "local_file" "AnsibleInventory" {
+    content = templatefile("inventory.tmpl",
+    {
+        ascs-hostname = module.ascs.fqdn,
+        ascs-ip = module.ascs.ip_address,
+        appserver-hostnames = module.appserver.*.fqdn,
+        appserver-ips = module.appserver.*.ip_address,
+        hdb-hostname = module.hdb.fqdn,
+        hdb-ip = module.hdb.ip_address
+    }
+    )
+    filename = "inventory"
+}
