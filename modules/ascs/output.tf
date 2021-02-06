629ad2e59320b93761d99c4f8b28c96b784fa1b9
Florian Lauchner <florian.lauchner@gmail.com>
2021-02-06 16:03:07 +0100

    Outputs not needed

--- modules/ascs/output.tf
+++
@@ -1,6 +0,0 @@
-output "ip_address" {
-  value = aws_instance.sap_instance.private_ip
-}
-output "fqdn" {
-  value = aws_route53_record.servername.fqdn
-}
--- modules/hana_singlenode/output.tf
+++
@@ -1,6 +0,0 @@
-output "ip_address" {
-  value = aws_instance.instance.private_ip
-}
-output "fqdn" {
-  value = aws_route53_record.servername.fqdn
-}
